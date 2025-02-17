0daf3a42588bf1addf838818105a474dd366efc7
Yawson-Enoch <enochgybex25@gmail.com>
2025-02-17 13:46:17 +0000

    feat: add dashboard index page

--- src/app/pages/dashboard/dashboard-index/dashboard-index.component.ts
+++ src/app/pages/dashboard/dashboard-index/dashboard-index.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { ListChecks, ListTodo, LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-dashboard-index',
-  imports: [],
-  templateUrl: './dashboard-index.component.html'
+  imports: [LucideAngularModule],
+  templateUrl: './dashboard-index.component.html',
 })
 export class DashboardIndexComponent {
-
+  protected readonly ListTodoIcon = ListTodo;
+  protected readonly ListChecksIcon = ListChecks;
 }
